2cbf9f396791e1f2b2150ac590aa7573c3b9ccb4
sss2013 <kshi9478@gmail.com>
2024-09-25 13:45:51 +0900

    2024 웹 프로그래밍 3주차 보강

---
+++ src/main/kotlin/kr/ac/kumoh/s20190645/w20240302css/W24w0302cssApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20190645.w20240302css
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W24w0302cssApplication
+
+fun main(args: Array<String>) {
+	runApplication<W24w0302cssApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20190645/w20240302css/W24w0302cssApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20190645.w20240302css
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W24w0302cssApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
